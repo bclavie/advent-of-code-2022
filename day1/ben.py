e9fb0f5bb0e795607724ff10717ff48210c2a27e
Ben Clavie <ben.clavie@brightnetwork.co.uk>
2022-12-01 13:30:06 +0000

    1 ben documentation day

--- day1/ben.py
+++ day1/ben.py
@@ -1,21 +1,28 @@
 def extract_calories(input_file: str = "input.txt") -> list[int]:
+    """List comprehension to extract per-elf calorie sum for the input file"""
     with open(input_file) as f:
         calories = [
-            sum([int(cal) for cal in cals.split("\n")])
-            for cals in f.read().split("\n\n")
+            sum(
+                [int(cal) for cal in cals.split("\n")]
+            )  # Sum the line-by-line calorie value for an individual elf
+            for cals in f.read().split(
+                "\n\n"
+            )  # Split the input on double-line breaks to create a list of list of values for each elf
         ]
 
     return calories
 
 
 def solve_part_1(calories: list[int]) -> int:
+    """Simple helper function to find the highest calorie count"""
     return max(calories)
 
 
 def solve_part_2(calories: list[int]) -> int:
-    sorted_calories = sorted(calories)
-    top_3 = sorted_calories[-3:]
-    top_3_sum = sum(top_3)
+    """Extract and sum the top 3 highest values in a list"""
+    sorted_calories = sorted(calories)  # Sort the list in an ascending order
+    top_3 = sorted_calories[-3:]  # Retrieve the final 3 elements
+    top_3_sum = sum(top_3)  # Sum the final 3 elements
 
     return top_3_sum
 
