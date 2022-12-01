20e6b7d05cfe5b572b93fb6c0a0e817fbf82df3b
Marcel Lachance <marspl17@gmail.com>
2022-12-01 18:27:02 -0500

    Mars' C# solution for day 1

---
+++ day1/mars.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var total = new List<int>();
+int currentElf = 0;
+
+foreach (var line in input)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        total.Add(currentElf);
+        currentElf = 0;
+    }
+    else
+    {
+        currentElf += int.Parse(line);
+    }
+}
+
+total.Add(currentElf);
+
+Console.WriteLine("Part 1: The elf with the most calories has " + total.OrderByDescending(x => x).Take(1).Sum() + " calories.");
+
+Console.WriteLine("Part 2: The three elves with the most calories have " + total.OrderByDescending(x => x).Take(3).Sum() + " calories.");
